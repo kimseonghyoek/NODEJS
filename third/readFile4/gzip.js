c90bf6d66e6e0de508f7cff6d0f4837563de0357
Kimseonghyoek <cilo1223@naver.com>
2022-02-07 04:02:00 +0900

    add gzip

---
+++ third/readFile4/gzip.js
@@ -0,0 +1,7 @@
+const zlib = require('zlib');
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./readme.txt');
+const zlibStream = zlib.createGzip();
+const writeStream = fs.createWriteStream('./readme.txt.gz');
+readStream.pipe(zlibStream).pipe(writeStream);
