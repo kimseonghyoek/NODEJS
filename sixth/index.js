2384a06e16b890b71dbe0da0fcfba67532f127cd
Kimseonghyoek <cilo1223@naver.com>
2022-03-09 04:04:03 +0900

    add middleware2

--- sixth/index.js
+++ sixth/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookiParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const path = require('path');
 
+dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+app.use(cookiParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
 
 app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됩니다.');
@@ -22,4 +43,4 @@ app.use((err, req, res, next) => {
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
-})
+});
